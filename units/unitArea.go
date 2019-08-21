8683af6de927cf9eb78d3dc3978d18208397b843
Nick Wells <wells.nicholas@gmail.com>
2019-08-21 15:44:13 +0100

    added the are (10 square metres) and some comments

--- units/unitArea.go
+++ units/unitArea.go
@@ -13,17 +13,24 @@ var SquareMetreUnit = Unit{0, 0, 1, UnitOfArea, "m\u00B2", UnitOfArea.BaseUnitNa
 
 // AreaNames maps names to units of area
 var AreaNames = map[string]Unit{
+	// metric
 	"square-metre":     SquareMetreUnit,
 	"square metre":     SquareMetreUnit,
+	"are":              {0, 0, 100, UnitOfArea, "are", "are", "ares"},
 	"hectare":          {0, 0, 1e4, UnitOfArea, "ha", "hectare", "hectares"},
 	"square-kilometre": {0, 0, 1e6, UnitOfArea, "km\u00B2", "square kilometre", "square kilometres"},
 	"square kilometre": {0, 0, 1e6, UnitOfArea, "km\u00B2", "square kilometre", "square kilometres"},
-	"square-yard":      {0, 0, yard2ToMetre2, UnitOfArea, "yd\u00B2", "square yard", "square yards"},
-	"square yard":      {0, 0, yard2ToMetre2, UnitOfArea, "yd\u00B2", "square yard", "square yards"},
-	"square-mile":      {0, 0, yard2ToMetre2 * 1760 * 1760, UnitOfArea, "mi\u00B2", "square mile", "square miles"},
-	"square mile":      {0, 0, yard2ToMetre2 * 1760 * 1760, UnitOfArea, "mi\u00B2", "square mile", "square miles"},
-	"rood":             {0, 0, yard2ToMetre2 * 1210, UnitOfArea, "rood", "rood", "roods"},
-	"acre":             {0, 0, yard2ToMetre2 * 4840, UnitOfArea, "acre", "acre", "acres"},
-	"oxgang":           {0, 0, yard2ToMetre2 * 4840 * 15, UnitOfArea, "oxgang", "oxgang", "oxgangs"},
-	"Wales":            {0, 0, 20779 * 1e6, UnitOfArea, "Wales", "Area the size of Wales", "Areas the size of Wales"},
+
+	// Imperial / US
+	"square-yard": {0, 0, yard2ToMetre2, UnitOfArea, "yd\u00B2", "square yard", "square yards"},
+	"square yard": {0, 0, yard2ToMetre2, UnitOfArea, "yd\u00B2", "square yard", "square yards"},
+	"square-mile": {0, 0, yard2ToMetre2 * 1760 * 1760, UnitOfArea, "mi\u00B2", "square mile", "square miles"},
+	"square mile": {0, 0, yard2ToMetre2 * 1760 * 1760, UnitOfArea, "mi\u00B2", "square mile", "square miles"},
+	"perch":       {0, 0, yard2ToMetre2 * 30.25, UnitOfArea, "rood", "rood", "roods"},
+	"rood":        {0, 0, yard2ToMetre2 * 1210, UnitOfArea, "rood", "rood", "roods"},
+	"acre":        {0, 0, yard2ToMetre2 * 4840, UnitOfArea, "acre", "acre", "acres"},
+	"oxgang":      {0, 0, yard2ToMetre2 * 4840 * 15, UnitOfArea, "oxgang", "oxgang", "oxgangs"},
+
+	// Colloquial
+	"Wales": {0, 0, 20779 * 1e6, UnitOfArea, "Wa", "Wales", "Wales"},
 }

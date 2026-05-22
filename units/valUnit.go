fe0662ca977adec4f8fb3f876a260b68d9429a7e
Nick Wells <wells.nicholas@gmail.com>
2026-05-22 13:15:38 +0100

    feature: add a ValUnit.Format method
    This will allow more sophisticated formatting of ValUnits.
    
    Also, a lot of refactoring of the existing ValUnit.String method was done.

--- units/valUnit.go
+++ units/valUnit.go
@@ -2,6 +2,8 @@ package units
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/nickwells/mathutil.mod/v2/mathutil"
 )
@@ -13,27 +15,137 @@ type ValUnit struct {
 	U Unit
 }
 
-// String returns a string form of the ValUnit
-func (v ValUnit) String() string {
+// unitNames returns the pair of names (singular and plural) for the unit.
+func (v ValUnit) unitNames() (string, string) {
 	singularName := v.U.name
 	pluralName := v.U.namePlural
 
-	if v.U.alias != "" {
+	if v.U.alias != "" &&
+		v.U.alias != singularName &&
+		v.U.alias != pluralName {
 		singularName += " (" + v.U.alias + ")"
 		pluralName += " (" + v.U.alias + ")"
 	}
 
-	epsilon := 0.0000001
+	return singularName, pluralName
+}
+
+// effectiveVal returns the value or, if the value is within (10^-prec)/2 of
+// zero or one then those values are returned.
+func (v ValUnit) effectiveVal(prec int) float64 {
+	epsilon := math.Pow10(-prec) / 2 //nolint:mnd
 
 	if mathutil.AlmostEqual(v.V, 1.0, epsilon) {
-		return "1 " + singularName
+		return 1.0
 	}
 
 	if mathutil.AlmostEqual(v.V, 0.0, epsilon) {
-		return "0 " + pluralName
+		return 0.0
 	}
 
-	return fmt.Sprintf("%.5g %s", v.V, pluralName)
+	return v.V
+}
+
+// String returns a string form of the ValUnit
+func (v ValUnit) String() string {
+	singularName, name := v.unitNames()
+
+	const prec = 5
+
+	eVal := v.effectiveVal(prec)
+	if eVal == 1.0 {
+		name = singularName
+	}
+
+	return fmt.Sprintf("%.5g %s", eVal, name)
+}
+
+// makeFmtStart translates the fmt.State into the start of a format
+// string. This can then have the verb character added at the end to generate
+// the appropriate formatting string.
+func makeFmtStart(f fmt.State) string {
+	fmtStart := "%"
+
+	if f.Flag('+') {
+		fmtStart += "+"
+	}
+
+	if f.Flag('0') {
+		fmtStart += "0"
+	}
+
+	if f.Flag('#') {
+		fmtStart += "#"
+	}
+
+	if wid, ok := f.Width(); ok {
+		fmtStart += strconv.Itoa(wid)
+	}
+
+	if prec, ok := f.Precision(); ok {
+		fmtStart += "." + strconv.Itoa(prec)
+	}
+
+	return fmtStart
+}
+
+// Format provides a custom formatter for a ValUnit
+func (v ValUnit) Format(f fmt.State, verb rune) {
+	fmtStrStart := makeFmtStart(f)
+	stringFmt := fmtStrStart + "s"
+	numFmt := fmtStrStart + "f"
+	valFmt := fmtStrStart + "v"
+
+	switch verb {
+	case 's':
+		fmt.Fprintf(f, stringFmt, v.String())
+	case 'u':
+		singularName, name := v.unitNames()
+		nameWidth := max(len(singularName), len(name))
+		nameFmt := "%" +
+			strconv.Itoa(nameWidth) +
+			"." + strconv.Itoa(nameWidth) +
+			"s"
+		prec, _ := f.Precision()
+		eVal := v.effectiveVal(prec)
+		if eVal == 1.0 {
+			name = singularName
+		}
+
+		fmt.Fprintf(f, numFmt+" "+nameFmt, eVal, name)
+	case 'T':
+		fmt.Fprintf(f, stringFmt, "units.ValUnit")
+	case 'v':
+		if f.Flag('#') {
+			fmt.Fprint(f, "units.ValUnit")
+		}
+
+		fmt.Fprint(f, "{")
+
+		if f.Flag('#') || f.Flag('+') {
+			fmt.Fprint(f, "V:")
+		}
+
+		fmt.Fprintf(f, numFmt, v.V)
+
+		if f.Flag('#') {
+			fmt.Fprint(f, ", ")
+		} else {
+			fmt.Fprint(f, " ")
+		}
+
+		if f.Flag('#') || f.Flag('+') {
+			fmt.Fprint(f, "U:")
+		}
+
+		fmt.Fprintf(f, valFmt, v.U)
+
+		fmt.Fprint(f, "}")
+	default:
+		_, _ = f.Write([]byte("%!" +
+			string(verb) +
+			"(ValUnit=" + v.String() + ")"))
+	}
 }
 
 // convertFromBaseUnits converts a value expressed in the base Units into

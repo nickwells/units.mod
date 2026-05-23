d77ff82a23a1b0e2f1e09c93ddbc002b5b6a4b47
Nick Wells <wells.nicholas@gmail.com>
2026-05-23 11:45:26 +0100

    test: added a test of GetTagNames

--- units/tags_test.go
+++ units/tags_test.go
@@ -1,6 +1,7 @@
 package units_test
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/nickwells/testhelper.mod/v2/testhelper"
@@ -24,4 +25,30 @@ func TestTagNote(t *testing.T) {
 		note := tc.tag.Notes()
 		testhelper.DiffString(t, tc.IDStr(), "note", note, tc.expNote)
 	}
+}
+
+func TestTagNames(t *testing.T) {
+	expTagNames := []string{
+		"SI",
+		"metric",
+		"historic",
+		"apothecary",
+		"imperial",
+		"US customary",
+		"colloquial",
+		"astronomical",
+		"computing",
+		"printing",
+		"physics",
+		"trigonometric",
+		"nautical",
+		"drink",
+		"dimensionless",
+	}
+	sort.Strings(expTagNames)
+
+	actTagNames := units.GetTagNames()
+	sort.Strings(actTagNames)
+
+	testhelper.DiffStringSlice(t, "GetTagNames", "", actTagNames, expTagNames)
 }
